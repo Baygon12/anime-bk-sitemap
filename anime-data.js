88319b15212a339e33cf07b06e8afa100b2a61ab
Baygon12 <kktownxx33@gmail.com>
2025-06-20 22:51:03 +0700

    Update anime-data.js

--- anime-data.js
+++ anime-data.js
@@ -2,7 +2,7 @@ const animeList = [
   {
     title: "Black Clover แบล็คโคลเวอร์ ตอนที่ 1-170",
     img: "https://i0.wp.com/anime-no1.com/wp-content/uploads/2023/12/Black-Clover.jpg?w=400&ssl=1",
-    link: "https://baygon12.github.io/anime-bk-sitemap/list.html",
+    link: "black-clover-thai.html",
     type: "พากย์ไทย"
   },
   {
