d0b6514e91e85a6bc9de2282f4f6b111b1d84e23
Baygon12 <kktownxx33@gmail.com>
2025-06-20 20:12:25 +0700

    Update anime-data.js

--- anime-data.js
+++ anime-data.js
@@ -1,9 +1,21 @@
-// anime-data.js
 const animeList = [
   {
-    title: "Black Clover แบล็คโคลเวอร์ ตอนที่ 1-170 พากย์ไทย",
+    title: "Black Clover แบล็คโคลเวอร์ ตอนที่ 1-170",
     img: "https://i0.wp.com/anime-no1.com/wp-content/uploads/2023/12/Black-Clover.jpg?w=400&ssl=1",
-    link: "https://baygon12.github.io/anime-bk-sitemap/list.html"
+    link: "https://baygon12.github.io/anime-bk-sitemap/list.html",
+    type: "พากย์ไทย"
   },
-  // เพิ่มรายการอื่นๆ
+  {
+    title: "Naruto นารูโตะ ตำนานวายุสลาตัน",
+    img: "https://i0.wp.com/anime-no1.com/wp-content/uploads/2023/11/naruto.jpg?w=400&ssl=1",
+    link: "https://example.com/naruto",
+    type: "ซับไทย"
+  },
+  {
+    title: "Your Name หลับตาฝัน ถึงชื่อเธอ",
+    img: "https://i0.wp.com/anime-no1.com/wp-content/uploads/2023/10/your-name.jpg?w=400&ssl=1",
+    link: "https://example.com/yourname",
+    type: "The Movie"
+  },
+  // เพิ่มเรื่องอื่นๆ ต่อจากนี้...
 ];
