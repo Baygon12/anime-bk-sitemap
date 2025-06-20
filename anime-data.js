4536360ef0d5dbb1fceee0855420958f001a6b27
Baygon12 <kktownxx33@gmail.com>
2025-06-20 19:41:44 +0700

    Create anime-data.js

---
+++ anime-data.js
@@ -0,0 +1,9 @@
+// anime-data.js
+const animeList = [
+  {
+    title: "Black Clover แบล็คโคลเวอร์ ตอนที่ 1-170 พากย์ไทย",
+    img: "https://i0.wp.com/anime-no1.com/wp-content/uploads/2023/12/Black-Clover.jpg?w=400&ssl=1",
+    link: "https://baygon12.github.io/anime-bk-sitemap/list.html"
+  },
+  // เพิ่มรายการอื่นๆ
+];
