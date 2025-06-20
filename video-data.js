3361ce68ac504ba5e1b726390f01e8d604387098
Baygon12 <kktownxx33@gmail.com>
2025-06-20 23:39:09 +0700

    Create video-data.js

---
+++ video-data.js
@@ -0,0 +1,14 @@
+const videoData = [
+  {
+    title: "One Piece ตอนที่ 1",
+    video: "https://drive.google.com/file/d/XXXXXXX/preview"
+  },
+  {
+    title: "One Piece ตอนที่ 2",
+    video: "https://cdn.example.com/op2.mp4"
+  },
+  {
+    title: "Naruto ตอนที่ 1",
+    video: "https://drive.google.com/file/d/YYYYYY/preview"
+  }
+];
