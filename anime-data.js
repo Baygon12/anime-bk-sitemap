743f5808aa13df8e248fec3485cd13372f7df224
Baygon12 <kktownxx33@gmail.com>
2025-06-22 19:41:18 +0700

    Update anime-data.js

--- anime-data.js
+++ anime-data.js
@@ -49,16 +49,17 @@ const animeList = [
     link: "One-Piece-Season-21-thai"
   },
   {
-    title: "Fights Break Sphere Season 4 สัประยุทธ์ทะลุฟ้า ภาค 4 ตอนที่ 1-24 ซับไทย",
-    img: "https://img2.pic.in.th/pic/Fights-Break-Sphere-Season-4-1_11zon.jpg",
-    type: "ซับไทย",
-    link: "Fights-Break-Sphere-Season4"
+    title: "Perfect World โลกอันสมบูรณ์แบบ ตอนที่ 1-221 ซับไทย",
+    img: "https://img5.pic.in.th/file/secure-sv1/Perfect-World-1.jpg",
+    type: "ตอนที่ 1-220 ซับไทย",
+    link: "Perfect-World"
   },
   {
-    title: "Fights Break Sphere Season 4 สัประยุทธ์ทะลุฟ้า ภาค 4 ตอนที่ 1-24 ซับไทย",
-    img: "https://img2.pic.in.th/pic/Fights-Break-Sphere-Season-4-1_11zon.jpg",
-    type: "ซับไทย",
-    link: "Fights-Break-Sphere-Season4"
+  {
+    title: "Perfect World โลกอันสมบูรณ์แบบ ตอนที่ 1-212 พากย์ไทย",
+    img: "https://img5.pic.in.th/file/secure-sv1/Perfect-World-1.jpg",
+    type: "ตอนที่ 1-212 พากย์ไทย",
+    link: "Perfect-World-thai"
   },
   // เพิ่มรายการอนิเมะได้เรื่อย ๆ
 ];
