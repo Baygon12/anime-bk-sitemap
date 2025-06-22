95ebc73de989d3c97cdae36aaf7044d21587d6c7
Baygon12 <kktownxx33@gmail.com>
2025-06-22 15:36:50 +0700

    Update anime-data.js

--- anime-data.js
+++ anime-data.js
@@ -2,13 +2,13 @@ const animeList = [
   {
     title: "Black Clover แบล็คโคลเวอร์ ตอนที่ 1-170 พากย์ไทย",
     img: "https://img5.pic.in.th/file/secure-sv1/bf26af8a102711cdc85dc1befc400c4e.jpg",
-    type: "พากย์ไทย",
+    type: "ตอนที่ 1-170 พากย์ไทย",
     link: "black-clover-thai"
   },
   {
     title: "Fights Break Sphere Season 5 สัประยุทธ์ทะลุฟ้า ภาค 5 ตอนที่ 1-152 ซับไทย",
     img: "https://img2.pic.in.th/pic/Fights-Break-Sphere-Season-5-1_11zon3cf7c93b89dc03bb.jpg",
-    type: "ซับไทย",
+    type: "ตอนที่ 1-152 ซับไทย",
     link: "Fights-Break-Sphere-Season-5"
   },
   {
